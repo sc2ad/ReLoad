48a5c56a2ea73bab4bdc53c4affa4f7f201fc002
Liskuxx <lelea500@gmail.com>
2019-12-04 00:12:53 +1300

    Still a lot to do yikes

--- WinReLoad/WinReLoad/Main.Designer.cs
+++ WinReLoad/WinReLoad/Main.Designer.cs
@@ -28,13 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(728, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1065, 595);
+            this.BackgroundImage = global::WinReLoad.Properties.Resources.BG;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "ReLoad";
@@ -43,6 +59,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ WinReLoad/WinReLoad/Main.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using Org.BouncyCastle.Cms;
+using Org.BouncyCastle.X509;
+using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Utilities;
+using Org.BouncyCastle.Crypto.Prng;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.X509.Store;
+using System.Security.Cryptography;
+using Org.BouncyCastle.Asn1.Cms;
+using System.Text.RegularExpressions;
+using Org.BouncyCastle.Utilities.IO.Pem;
+
+using OpenSsl = Org.BouncyCastle.OpenSsl;
+
+
+    
+
+
+
+namespace WinReLoad
+{
+    public partial class Form1 : Form
+    {
+        //VARIABLES
+        bool installed = false;
+        string modName = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileInfo FileCheck = new FileInfo("This PC\\Quest\\Internal shared storage\\Android\\data\\com.cloudheadgames.pistolwhip\files" + "\\mods");
+            if (FileCheck.FullName != modName)
+            {
+                Application.Exit();
+            }
+        }
+    }
+
+
+
+
+
+
+
+}
